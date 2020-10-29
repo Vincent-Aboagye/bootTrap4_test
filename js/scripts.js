c2070e648751a631030a4dff3355f8941a319b0e
Vynx <68636740+Vincent-Aboagye@users.noreply.github.com>
2020-10-29 10:19:39 +0000

    Gulp part 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $("#myCarousel").carousel({interval: 2000});
-    $("#carouselButton").click(function(){
+    $("#carouselButton").onclick(function(){
         if ($("#carouselButton").children("span").hasClass("fa-pause")){
             $("#myCarousel").carousel('pause');
             $("#carouselButton").children("span").removeClass("fa-pause");
             $("#carouselButton").children("span").addClass("fa-play");
         }
-        else{
+        else {
             $("#myCarousel").carousel('cycle');
             $("#carouselButton").children("span").removeClass("fa-play");
             $("#carouselButton").children("span").addClass("fa-pause");
@@ -14,10 +14,10 @@ $(document).ready(function(){
         
     });
 
-    $("#loginButton").click(function(){
+    $("#loginButton").onclick(function(){
         $("#loginModal").modal('toggle');
     });
-    $("#reserveButton").click(function(){
+    $("#reserveButton").onclick(function(){
         $("#reservationModal").modal('toggle');
     });
 
